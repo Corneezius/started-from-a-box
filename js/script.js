069d0f688cbb4612064d689b4aaa62ef5c51cb96
Corneezius <cfr2116@columbia.edu>
2017-08-31 02:28:34 -0400

    add video to opening sequence

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
 		$('#rumble-credit').hide(0).fadeIn(1000);
+		$('#bgvid2')[0].play();
 
 		setTimeout(function(){
 			$('#rumble-credit').fadeOut(1000);
 		}, 1500);
 
 		setTimeout(function(){
+			$('#bgvid2').show();
+		}, 2550);
+
+		setTimeout(function(){
+			$('#bgvid2').hide();
+		}, 5900);
+
+		setTimeout(function(){
 			$('#project-title').show();
 			$('#artist-name').show();
 			$('#bars').show();
-		}, 3000);
+		}, 6000);
 
 	  // setTimeout(function(){
 		// 	$('#project-title').hide();
