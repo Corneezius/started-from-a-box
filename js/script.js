892d82670a35eaa597ab65902b7f6d6402a9b6d4
Corneezius <cfr2116@columbia.edu>
2017-09-12 14:48:15 -0400

    transfer files

--- js/script.js
+++ js/script.js
@@ -1,33 +1,68 @@
 $(document).ready(function() {
 		$('#rumble-credit').hide(0).fadeIn(1000);
-		$('#bgvid2')[0].play();
 
 		setTimeout(function(){
 			$('#rumble-credit').fadeOut(1000);
 		}, 1500);
 
-		setTimeout(function(){
-			$('#bgvid2').show();
-		}, 2550);
-
-		setTimeout(function(){
-			$('#bgvid2').hide();
-		}, 5900);
+		// setTimeout(function(){
+		// 	$('#bgvid2')[0].play();
+		// 	$('#bgvid2').show();
+		// }, 2550);
+		//
+		// setTimeout(function(){
+		// 	$('#bgvid2').hide();
+		// }, 5500);
 
 		setTimeout(function(){
 			$('#project-title').show();
 			$('#artist-name').show();
 			$('#bars').show();
-		}, 6000);
+		}, 3000);
 
-	  // setTimeout(function(){
-		// 	$('#project-title').hide();
-		// 	$('#artist-name').hide();
-	  // }, 6000);
+	  setTimeout(function(){
+			$('#project-title').hide();
+			$('#artist-name').hide();
+			$('#bars').hide();
+	  }, 6000);
 
-	// 	//
-  // setTimeout(function(){
-	// 	  $('body').addClass('loaded');
-	// 		$('body').removeClass("noscroll");
-	// 	}, 6400);
+  setTimeout(function(){
+			$('body').removeClass("noscroll");
+			$('body').addClass('loaded');
+			$('.btn-toggle').show();
+		}, 6500);
  });
+
+ $('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
+
+$(document).ready(function(){
+    $("#push").click(function(){
+        $('html, body').animate({ scrollTop: 0 }, 0);
+        $('#bgvid').show();
+        $('.navbar-custom').css('background-color','black');
+        $('header').css('background','black');
+        $('#bgvid')[0].play();
+
+        setTimeout(function(){
+      		$('#bgvid').fadeOut(500);
+          $('header').css('background','');
+          $('.navbar-custom').css('background-color', '');
+      		// $('#ninety-six').fadeOut(1000);
+      	}, 55000);
+    });
+});
